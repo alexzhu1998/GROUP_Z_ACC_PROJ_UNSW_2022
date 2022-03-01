484312c1c92601adcb90ba04b25d980d119d7d13
alexzhu1998 <alexzhu1998_aus@hotmail.com>
2022-03-01 23:37:52 +1100

    added standardised

--- exploratory.R
+++ exploratory.R
@@ -24,20 +24,25 @@ vec_of_attr <- c("Total_Cmp_per",
                  "Blocks_Pass")
 
 ## Took average of all 90s columns
-df$`90s_avg` <- rowMeans(df %>% select("90s","90s.x","90s.y"))
+df$`90s_avg` <- rowMeans(df %>% select("90s","90s.x","90s.y")) * 90
+df$`90s_avg` <- ifelse(df$`90s_avg` < 0.1,0,df$`90s_avg`)
+df <- filter(df,`90s_avg` > 0)
+df[vec_of_attr] <- df[vec_of_attr]/df$`90s_avg`
+
+
 
 
 ## Standardised
-df[vec_of_attr] <- df[vec_of_attr]/df$`90s_avg`
 
-str<- paste(paste0(vec_of_attr,' = mean(',vec_of_attr, ')'), collapse=",")
+# 
+# str<- paste(paste0(vec_of_attr,' = mean(',vec_of_attr, ')'), collapse=",")
+# 
+# 
+# nation <- df %>% 
+#     group_by(Nation) %>%
+#     summarise(eval(parse(text = str)))
 
-
-nation <- df %>% 
-    group_by(Nation) %>%
-    summarise(eval(parse(text = str)))
-
-league <- df %>% 
+league <- df %>%
     group_by(League)  %>%
     summarise(Total_Cmp_per = mean(Total_Cmp_per,na.rm=T),
               Short_Cmp_per = mean(Short_Cmp_per,na.rm=T),
@@ -51,9 +56,9 @@ league <- df %>%
               Blocks_Blocks = mean(Blocks_Blocks,na.rm=T),
               Blocks_Sh = mean(Blocks_Sh,na.rm=T),
               Blocks_ShSv = mean(Blocks_ShSv,na.rm=T),
-              Blocks_Pass = mean(Blocks_Pass,na.rm=T)) 
+              Blocks_Pass = mean(Blocks_Pass,na.rm=T))
 
-nation <- df %>% 
+nation <- df %>%
     group_by(Nation)  %>%
     summarise(Total_Cmp_per = mean(Total_Cmp_per,na.rm=T),
               Short_Cmp_per = mean(Short_Cmp_per,na.rm=T),
@@ -67,8 +72,12 @@ nation <- df %>%
               Blocks_Blocks = mean(Blocks_Blocks,na.rm=T),
               Blocks_Sh = mean(Blocks_Sh,na.rm=T),
               Blocks_ShSv = mean(Blocks_ShSv,na.rm=T),
-              Blocks_Pass = mean(Blocks_Pass,na.rm=T)) 
+              Blocks_Pass = mean(Blocks_Pass,na.rm=T))
 
 
 
+## 
+## Clean outliers from each column
+## 
+## Correlation heatmap
 
