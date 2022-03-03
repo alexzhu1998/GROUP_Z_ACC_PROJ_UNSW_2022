a1b87c7af7355019d54b69253d89bc2e651e8fc1
CheekyWinf <matthew.winfred@gmail.com>
2022-03-03 23:31:17 +1100

    :p

--- exploratory.R
+++ exploratory.R
@@ -1,9 +1,13 @@
 load("data/merged_league.RData")
 load("data/merged_tourn.RData")
-
+install.packages("gbm")
 
 library(dplyr)
 library(corrplot)
+library(ggplot2)
+library(gbm)
+
+
 df <- PLAYER_league_non_goal_salary
 
 #Edit in a new column that takes first two characters from position
@@ -128,3 +132,40 @@ cormat <- cor(temp_df[41:60], method = "pearson")
 cormat <- corrplot(cormat, method = "number")
 cormat <- cor(temp_df[61:67], method = "pearson")
 cormat <- corrplot(cormat, method = "number")
+
+
+##Distribution of salary
+ggplot(df)+
+    geom_histogram(aes(x = Annualized_Salary, y = ..density..), color = "black", fill="#33AFFF")+
+    labs(x = "Annualised Salary", y = "Density", title = "Distribution of Annualised Salary")+
+    theme_bw() +
+    theme(axis.text=element_text(size=9.5), axis.title=element_text(size=13, face = "bold"), plot.title = element_text(size=16, face = "bold"), plot.subtitle=element_text(size=13))
+
+
+##Distribution of salary
+ggplot(PLAYER_league_non_goal_salary)+
+    geom_histogram(aes(x = `Annualized Salary`, y = ..density..), color = "black", fill="#33AFFF")+
+    labs(x = "Annualised Salary", y = "Density", title = "Distribution of Annualised Salary")+
+    theme_bw() +
+    theme(axis.text=element_text(size=9.5), axis.title=element_text(size=13, face = "bold"), plot.title = element_text(size=16, face = "bold"), plot.subtitle=element_text(size=13))
+
+
+
+gbmFit.param <- gbm(Annualized_Salary ~., data = df[,-c(1,2,3,4,5,71)], distribution = "gaussian", cv.fold = 10, n.trees = 10000, interaction.depth = 1, shrinkage = 0.01)
+gbmFit.param
+
+min <- which.min(gbmFit.param$cv.error)
+min
+gbm.perf(gbmFit.param, method = "cv")
+
+gbmFit <- gbm(Annualized_Salary ~., data = df[,-c(1,2,3,4,5,71)], distribution = "gaussian", n.trees = min, interaction.depth = 1, shrinkage = 0.01)
+
+summary(gbmFit)
+
+
+
+
+
+
+
+
