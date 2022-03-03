2091fc2ed8b7b1c31be4defcf9d6044d5c0193c8
CheekyWinf <matthew.winfred@gmail.com>
2022-03-04 05:26:28 +1100

    Model

--- exploratory.R
+++ exploratory.R
@@ -4,6 +4,8 @@ load("data/merged_tourn.RData")
 
 library(dplyr)
 library(corrplot)
+library(ggplot2)
+
 df <- PLAYER_league_non_goal_salary
 
 #Edit in a new column that takes first two characters from position
@@ -132,3 +134,36 @@ corrplot(cormat, method = "number")
 cor_df <- df[,keep]
 cormat <- cor(cor_df, method = "pearson")
 corrplot(cormat, method = "number")
+
+##Distribution of salary (including RFL)
+ggplot(df)+
+    geom_histogram(aes(x = Annualized_Salary, y = ..density..), color = "black", fill="#33AFFF")+
+    labs(x = "Annualised Salary", y = "Density", title = "Distribution of Annualised Salary")+
+    theme_bw() +
+    theme(axis.text=element_text(size=9.5), axis.title=element_text(size=13, face = "bold"), plot.title = element_text(size=16, face = "bold"), plot.subtitle=element_text(size=13))
+
+
+
+
+
+gbmFit.param <- gbm(Annualized_Salary ~., data = df[,-c(1,2,3,4,5,71)], distribution = "gaussian", cv.fold = 10, n.trees = 10000, interaction.depth = 1, shrinkage = 0.01)
+gbmFit.param
+
+min <- which.min(gbmFit.param$cv.error)
+min
+gbm.perf(gbmFit.param, method = "cv")
+
+gbmFit <- gbm(Annualized_Salary ~., data = df[,-c(1,2,3,4,5,71)], distribution = "gaussian", n.trees = min, interaction.depth = 1, shrinkage = 0.01)
+
+summary(gbmFit)
+
+
+
+
+
+
+
+
+
+
+
