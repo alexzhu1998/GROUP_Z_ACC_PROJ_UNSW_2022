222a31ba7c9435297876b017f1c0d7a5c873730a
alexzhu1998 <alexzhu1998_aus@hotmail.com>
2022-03-06 00:19:52 +1100

    cleaning up exploratory.R moved preprocessing functions to merge_data.R

--- R_files/exploratory.R
+++ R_files/exploratory.R
@@ -8,10 +8,7 @@ library(ggplot2)
 df <- PLAYER_league_non_goal_salary
 
 #Edit in a new column that takes first two characters from position
-df['Pos_new'] <- substr(df$Pos,1,2)
 
-
-colnames(df) <- gsub(" ","_", colnames(df))
 
 # Alex --------------------------------------------------------------------
 
@@ -35,11 +32,7 @@ vec_of_attr <- c("Total_Cmp%",
 
 pos <- c("DF","MF","FW")
 
-## Took average of all 90s columns
-df$`90s_avg` <- rowMeans(df %>% select("90s","90s.x","90s.y")) * 90
-df$`90s_avg` <- ifelse(df$`90s_avg` < 0.1,0,df$`90s_avg`)
-df <- filter(df,`90s_avg` > 0)
-df[vec_of_attr] <- df[vec_of_attr]/df$`90s_avg`
+
 
 
 
@@ -53,42 +46,8 @@ l <- df %>%
     summarise(across(vec_of_attr,~mean(.x,na.rm=T)))
 
 
-## Clean Missing Data from each column
-
-summary(df)
-
-# Replaced all negative values with 0
-for (c in colnames(df))
-    df[[c]] <- replace(df[[c]],which(df[[c]] <0) ,0) 
 
 
-# Replace all NA with 0
-for (c in colnames(df))
-    df[[c]] <- replace(df[[c]],which(is.na(df[[c]])) ,0) 
-
-
-summary(df)
-
-
-
-table(subset(df,is.na(`Standard_G/Sh`))$Pos)
-
-
-# for (i in colnames(df)) {
-#     for (j in colnames(df)) {
-#         if (i == j) {
-#             next
-#         } else {
-#             if (all(!is.na(df[[i]])) & all(!is.na(df[[j]])) & sum(df[[i]] == df[[j]])/nrow(df) > 0.9) {
-#                 print(paste(i,j))
-#             }
-#         }
-#     }
-# }
-
-df<- df %>% select(-c("90s","90s.x","90s.y"))
-
-df<- df %>% select(-c("Position", "Country","Pos"))
 
 nonRFL <- filter(df,League != "RFL")
 RFL <- filter(df,League == "RFL")
