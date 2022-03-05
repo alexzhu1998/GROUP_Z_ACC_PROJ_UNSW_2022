1fda9ee5ddda7751ee99bd6e46d72fcc1205a3ea
CheekyWinf <matthew.winfred@gmail.com>
2022-03-05 22:43:24 +1100

    Model

--- exploratory.R
+++ exploratory.R
@@ -5,6 +5,7 @@ load("data/merged_tourn.RData")
 library(dplyr)
 library(corrplot)
 library(ggplot2)
+library(gbm)
 
 df <- PLAYER_league_non_goal_salary
 
@@ -142,28 +143,72 @@ ggplot(df)+
     theme_bw() +
     theme(axis.text=element_text(size=9.5), axis.title=element_text(size=13, face = "bold"), plot.title = element_text(size=16, face = "bold"), plot.subtitle=element_text(size=13))
 
+## DO NOT CHANGE COLUMN ORDER -> CHANGING COLUMN ORDER WILL REQUIRE THE FOLLOWING CODE TO BE MODIFIED ##
 
-
-
-
-gbmFit.param <- gbm(Annualized_Salary ~., data = df[,-c(1,2,3,4,5,71)], distribution = "gaussian", cv.fold = 10, n.trees = 10000, interaction.depth = 1, shrinkage = 0.01)
+#Fit boosting model on annualised salary dataset with non RFL football leagues and ALL predictors
+gbmFit.param <- gbm(Annualized_Salary ~., data = df[df['League'] != "RFL",-c(1,2,3,4,5,71)], distribution = "gaussian", cv.fold = 10, n.trees = 10000, interaction.depth = 1, shrinkage = 0.01)
 gbmFit.param
 
 min <- which.min(gbmFit.param$cv.error)
 min
 gbm.perf(gbmFit.param, method = "cv")
 
-gbmFit <- gbm(Annualized_Salary ~., data = df[,-c(1,2,3,4,5,71)], distribution = "gaussian", n.trees = min, interaction.depth = 1, shrinkage = 0.01)
+gbmFit <- gbm(Annualized_Salary ~., df[df['League'] != "RFL",-c(1,2,3,4,5,71)], distribution = "gaussian", n.trees = min, interaction.depth = 1, shrinkage = 0.01)
 
 summary(gbmFit)
 
+gbm.predict = predict(gbmFit, newdata = df[,-c(1,2,3,4,5,70,71)], n.trees = min, type = "response")
 
 
+#New dataframe with correlated vairables removed + identifiable attributes
+cor_df_merge <- cbind(cor_df,df[,c("Player","Nation","Pos_new","League","Squad")])
 
+#MF model
+gbmFit.param_MF <- gbm(Annualized_Salary ~., data = cor_df_merge[(cor_df_merge['League'] != "RFL") & (cor_df_merge['Pos_new'] == "MF"),-c(19,20,21,22,23)], distribution = "gaussian", cv.fold = 10, n.trees = 10000, interaction.depth = 1, shrinkage = 0.01)
+gbmFit.param_MF
 
+min_MF <- which.min(gbmFit.param_MF$cv.error)
+min_MF
+gbm.perf(gbmFit.param_MF, method = "cv")
 
+gbmFit_MF <- gbm(Annualized_Salary ~., cor_df_merge[(cor_df_merge['League'] != "RFL") & (cor_df_merge['Pos_new'] == "MF"),-c(19,20,21,22,23)], distribution = "gaussian", n.trees = min_MF, interaction.depth = 1, shrinkage = 0.01)
 
+summary(gbmFit_MF)
 
+gbm.predict_MF = predict(gbmFit_MF, newdata = cor_df_merge[(cor_df_merge['Pos_new'] == "MF"),-c(17, 19,20,21,22,23)], n.trees = min_MF, type = "response")
+hist(gbm.predict_MF)
 
+#DF model
+gbmFit.param_DF <- gbm(Annualized_Salary ~., data = cor_df_merge[(cor_df_merge['League'] != "RFL") & (cor_df_merge['Pos_new'] == "DF"),-c(19,20,21,22,23)], distribution = "gaussian", cv.fold = 10, n.trees = 10000, interaction.depth = 1, shrinkage = 0.01)
+gbmFit.param_DF
 
+min_DF <- which.min(gbmFit.param_DF$cv.error)
+min_DF
+gbm.perf(gbmFit.param_DF, method = "cv")
 
+gbmFit_DF <- gbm(Annualized_Salary ~., cor_df_merge[(cor_df_merge['League'] != "RFL") & (cor_df_merge['Pos_new'] == "DF"),-c(19,20,21,22,23)], distribution = "gaussian", n.trees = min_DF, interaction.depth = 1, shrinkage = 0.01)
+
+summary(gbmFit_DF)
+
+gbm.predict_DF = predict(gbmFit_DF, newdata = cor_df_merge[(cor_df_merge['Pos_new'] == "DF"),-c(17, 19,20,21,22,23)], n.trees = min_DF, type = "response")
+hist(gbm.predict_DF)
+
+#FW model
+gbmFit.param_FW <- gbm(Annualized_Salary ~., data = cor_df_merge[(cor_df_merge['League'] != "RFL") & (cor_df_merge['Pos_new'] == "FW"),-c(19,20,21,22,23)], distribution = "gaussian", cv.fold = 10, n.trees = 10000, interaction.depth = 1, shrinkage = 0.01)
+gbmFit.param_FW
+
+min_FW <- which.min(gbmFit.param_FW$cv.error)
+min_FW
+gbm.perf(gbmFit.param_FW, method = "cv")
+
+gbmFit_FW <- gbm(Annualized_Salary ~., cor_df_merge[(cor_df_merge['League'] != "RFL") & (cor_df_merge['Pos_new'] == "FW"),-c(19,20,21,22,23)], distribution = "gaussian", n.trees = min_FW, interaction.depth = 1, shrinkage = 0.01)
+
+summary(gbmFit_FW)
+
+gbm.predict_FW = predict(gbmFit_FW, newdata = cor_df_merge[(cor_df_merge['Pos_new'] == "FW"),-c(17, 19,20,21,22,23)], n.trees = min_FW, type = "response")
+hist(gbm.predict_FW)
+
+#Actual salary histograms
+hist(df$Annualized_Salary[(df['League'] != "RFL") & (df['Pos_new'] == "MF")], breaks = 20)
+hist(df$Annualized_Salary[(df['League'] != "RFL") & (df['Pos_new'] == "DF")], breaks = 20)
+hist(df$Annualized_Salary[(df['League'] != "RFL") & (df['Pos_new'] == "FW")], breaks = 20)
