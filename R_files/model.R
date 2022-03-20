7a37e81df7eaad17667ffb036beb4be10507b032
CheekyWinf <matthew.winfred@gmail.com>
2022-03-20 15:25:33 +1100

    baedan

--- R_files/model.R
+++ R_files/model.R
@@ -287,7 +287,6 @@ rarita.df <- rarita.players %>%
 
 rarita.fw <- rarita.players %>%
     filter(Pos_new == 'FW') %>%
-    filter(quantile(Annualized_Salary, select.quantile) < Annualized_Salary)%>%
     select(Player, Annualized_Salary, gbm.predict_FW, Pos_new)%>%
     mutate(salary.ratio = gbm.predict_FW/Annualized_Salary)%>%
     arrange(desc(salary.ratio))
@@ -325,3 +324,12 @@ par.df.DF <- partial(gbmFit_DF, pred.var = c('Expected_xG'), n.trees = min_DF)
 par.df.DF <- partial(gbmFit_DF, pred.var = c('xA'), n.trees = min_DF)
 par.df.DF <- partial(gbmFit_DF, pred.var = c('Tackles_Tkl'), n.trees = min_DF)
 autoplot(par.df.DF, contour = TRUE)
+
+
+
+national.team.stats <- national.team %>%
+    group_by(Pos_new) %>%
+    summarise(Score = mean(Expected_Salary))
+
+
+
