5495081eca2e8f967bc4178b443d8a9ceb7f01d5
alexzhu1998 <alexzhu1998_aus@hotmail.com>
2022-03-21 00:27:21 +1100

    abc

--- R_files/filter_pipeline_exploratory.R
+++ R_files/filter_pipeline_exploratory.R
@@ -40,10 +40,11 @@ keep_goal <- c("Player","Nation","Pos_new","League",'Age','Playing_Time_90s','Pe
 
 #Player cormat
 temp_df <- df%>% select(-c("Player","Nation","Pos_new","League","Squad"))
-cor_df <- df[,keep_player]
+cor_df <- df[,c('Age','Tackles_Tkl','Vs_Dribbles_Att','Pressures_%','Blocks_Sh','Int','Clr','Total_Cmp%','xA','Standard_SoT/90','Standard_Sh/90','Standard_G/SoT','Standard_Dist','Standard_FK','Performance_PK','Expected_xG','Annualized_Salary','90s_avg')]
+stopifnot(dim(cor_df) == c(5500,18))
 
 gk_df <- PLAYER_league_goal_salary[,c('Age','Playing_Time_90s','Performance_GA90','Performance_Saves','Performance_Save%','W','D','L','Performance_CS','Performance_CS%','Penalty_Kicks_PKA','Penalty_Kicks_PKsv','Penalty_Kicks_PKm','Penalty_Kicks_Save%','Annualized_Salary','Player','Nation','League','Squad','Country')]
-
+stopifnot(dim(gk_df) == c(5500,18))
 
 player_tourn_df <- PLAYER_tourn_non_goal[,keep_player[!keep_player == 'Annualized_Salary']]
 gk_tourn_df <- PLAYER_tourn_goal[keep_goal[!keep_goal == 'Annualized_Salary']]
