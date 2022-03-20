8b042a654e61b29b82fad037181f575f962508a1
aidanyeohh <99695936+aidanyeohh@users.noreply.github.com>
2022-03-20 14:37:04 +1100

    match model updated

--- R_files/model.R
+++ R_files/model.R
@@ -213,9 +213,9 @@ team_stats <- team_stats %>% arrange(`2021 Tournament Place`, descending = T)
 
 plot(team_stats$`2021 Tournament Place`, team_stats$DF_score)
 
-#write.csv(team_stats,"data/match_model_data.csv")
+write.csv(team_stats,"data/match_model_data.csv")
 
-model_data <- read.csv("data/match_model.csv")
+# model_data <- read.csv("data/match_model.csv")
 
 model_data$Outcome[model_data$Outcome == "Win"] <- 1
 model_data$Outcome[model_data$Outcome == "Lose"] <- 0
@@ -256,7 +256,7 @@ mean(gbm.match.predict[247:492])
 
 
 # Raritian players updated table ------------------------------------------
-gbm.vector <- c(gbm.predict_DF,gbm.predict_FW,gbm.predict_GK,gbm.predict_MF)
+# gbm.vector <- c(gbm.predict_DF,gbm.predict_FW,gbm.predict_GK,gbm.predict_MF)
 column.names <- c('Player','Annualized_Salary','Expected_Salary','Salary_Ratio')
 player.names <- cor_df_merge[,c('Player','Annualized_Salary','Pos_new','Nation')]
 
